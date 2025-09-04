b8a1fb47698c92895aab4ad124a145df4eecfb00
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-09-04 21:31:59 +0200

    changed file name

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/components/Header";
 import { Home } from "@/components/Home";
 import { Features } from "@/components/Features";
-import { CTA } from "@/components/CTA";
+import { CTA } from "@/components/Footer";
 
 const Index = () => {
   return (
